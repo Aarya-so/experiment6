bc4df3de4bddbff8003a14f021c6afcd9e8514a8
LAPTOP-TE4LTDBT\LENOVO <kiranm@exploresys.com>
2025-10-07 23:41:50 +0530

    Add React app for Experiment 6

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is my first React Application</h1>
+        <p>Learn React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
